3a707d310f37d5cf6a3ae44fcf1f028491f61ce9
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2023-04-27 19:41:53 +0100

    removed convolutions, integrated all transformations
    10-15% training accuracy, 3% test accuracy

--- assessment.py
+++ assessment.py
@@ -14,10 +14,10 @@ type(train_ds)
 
 print(type(train_ds))
 
-#transform images
+#transform images to the chosen dimension (they are just squeezed)
 def scale_resize_image(image, label):
     image = tf.image.convert_image_dtype(image, tf.float32) # equivalent to dividing image pixels by 255
-    image = tf.image.resize(image, (120, 120)) # Resizing the image to 120x120 dimension
+    image = tf.image.resize(image, (120, 120)) # Resizing the image to 224x224 dimention
     return (image, label)
 
 #converts images to greyscale
@@ -33,20 +33,27 @@ def crop_image(image, label):
 AUTO = tf.data.experimental.AUTOTUNE
 BATCH_SIZE = 256
 
-#training_ds = (train_ds.map(crop_image)
-#training_ds = (training_ds.map(rgb_convert))
-training_ds = (train_ds.map(scale_resize_image))
+training_ds = (train_ds.map(crop_image))
+training_ds = (training_ds.map(scale_resize_image))
+training_ds = (training_ds.map(rgb_convert))
 training_ds = training_ds.batch(BATCH_SIZE)
+
+testing_ds = (test_ds.map(scale_resize_image))
+testing_ds = testing_ds.map(rgb_convert)
+testing_ds = testing_ds.batch(BATCH_SIZE)
 
 #for example in training_ds:
 #  print(tf.shape(tf.expand_dims(example[0], axis = 0)))
 
+#display the resized images
+#fig = tfds.show_examples(training_ds, ds_info, rows = 4, cols = 4)
+
 model = keras.Sequential([
-    keras.Input((120, 120, 3)),
-    layers.Conv2D(100, 3, activation='relu'),
-    layers.Flatten(),
-    layers.Dense(102),
+    keras.layers.Flatten(input_shape=(120,120,1)),
+    keras.layers.Dense(102, activation=tf.nn.softmax)
 ])
+
+model.summary()
 
 model.compile(
     optimizer = keras.optimizers.Adam(learning_rate = 0.001),
@@ -55,3 +62,4 @@ model.compile(
 )
 
 model.fit(training_ds, epochs = 5, verbose=2)
+model.evaluate(testing_ds)
