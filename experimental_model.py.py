1840e9148ce0905498ac1474178fe6f1071ac772
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2023-05-07 01:21:26 +0100

    refactored data normalisation

--- experimental_model.py.py
+++ experimental_model.py.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+# coding: utf-8
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -12,7 +12,6 @@ from tensorflow.keras.layers import BatchNormalization,MaxPool2D
 from tensorflow.keras.layers import GlobalAvgPool2D
 from tensorflow.keras import Model
 
-# creating the Conv-Batch Norm block
 
 def conv_bn(x, filters, kernel_size, strides=1):
   x = Conv2D(filters=filters, 
@@ -34,17 +33,14 @@ def sep_bn(x, filters, kernel_size, strides=1):
   x = BatchNormalization()(x)
   return x
 
-from re import X
-# model code
-
 input = keras.layers.Input(shape = (299,299,3))
-x = keras.layers.RandomFlip("horizontal_and_vertical")(input)
-x = keras.layers.RandomRotation(1)(x)
-x = keras.layers.RandomZoom(0.5)(x)
-x = keras.layers.RandomBrightness(0.2/255)(x)
-x = keras.layers.RandomTranslation(0.1, 0.1)(x)
-x = keras.layers.BatchNormalization()(x)  
-x = conv_bn(x, filters =32, kernel_size =3, strides=2)
+#x = keras.layers.RandomFlip("horizontal_and_vertical")(input)
+#x = keras.layers.RandomRotation(1)(x)
+#x = keras.layers.RandomZoom(0.5)(x)
+#x = keras.layers.RandomBrightness(0.2/255)(x)
+#x = keras.layers.RandomTranslation(0.1, 0.1)(x)
+#x = keras.layers.BatchNormalization()(x)  
+x = conv_bn(input, filters =32, kernel_size =3, strides=2)
 x = ReLU()(x)
 x = conv_bn(x, filters =64, kernel_size =3, strides=1)
 tensor = ReLU()(x)
@@ -107,38 +103,31 @@ output = x
 model = Model (inputs=input, outputs=output)
 model.summary()
 
+
 data, ds_info = tfds.load('oxford_flowers102',
                          with_info=True,
                           as_supervised=True,
                           shuffle_files = True)
 train_ds, valid_ds, test_ds = data['train'], data['validation'], data['test']
 
-def scale_resize_image(image, label):
-    image = tf.image.convert_image_dtype(image, tf.float32) # equivalent to dividing image pixels by 255
-    image = tf.image.resize(image, (299, 299)) # Resizing the image to 224x224 dimention
+def normalize_image(image, label):
+    image = tf.image.resize_with_crop_or_pad(image, 500, 500)
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    image = tf.image.resize(image, (299, 299))
     return (image, label)
 
-def crop_image(image, label):
-    image = tf.image.resize_with_crop_or_pad(image, 500, 500)
-    return (image, label)
 
 AUTO = tf.data.experimental.AUTOTUNE
-BATCH_SIZE = 32
+BATCH_SIZE = 8
 
-training_ds = (train_ds.map(crop_image))
-training_ds = (training_ds.map(scale_resize_image))
-training_ds = training_ds.batch(BATCH_SIZE)
+training_ds = (train_ds.map(normalize_image)).cache().batch(BATCH_SIZE)
 
-testing_ds = (test_ds.map(crop_image))
-testing_ds = (testing_ds.map(scale_resize_image))
-testing_ds = testing_ds.batch(BATCH_SIZE)
+testing_ds = (test_ds.map(normalize_image)).cache().batch(BATCH_SIZE)
 
-validation_ds = (valid_ds.map(crop_image))
-validation_ds = (validation_ds.map(scale_resize_image))
-validation_ds = validation_ds.batch(BATCH_SIZE)
+validation_ds = (valid_ds.map(normalize_image)).cache().batch(BATCH_SIZE)
 
 model.compile(
-    optimizer = keras.optimizers.Adam(learning_rate = 0.00005),
+    optimizer = keras.optimizers.Adam(learning_rate = 0.001),
     loss = keras.losses.SparseCategoricalCrossentropy(),
     metrics = ["accuracy"]
 )
@@ -151,6 +140,8 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     mode='max',
     save_best_only=True)
 
+
 model.fit(training_ds, validation_data = validation_ds, epochs = 700, verbose=2, callbacks = [model_checkpoint_callback])
 
 model.evaluate(testing_ds)
+
