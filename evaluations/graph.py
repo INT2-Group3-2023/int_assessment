5d1502c8c3c10d4e35fb575233bf60df5bbcb948
yaseenkhancs <68476234+yaseenkhancs@users.noreply.github.com>
2023-05-10 03:36:52 +0100

    removed bad tests, added graph plots

--- evaluations/graph.py
+++ evaluations/graph.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 plt.figure(figsize=(8, 8))
 
 # open files
-file1 = open("augmentation/with_augmentation.txt")
-file2 = open("augmentation/no_augmentation.txt")
+file1 = open("model depth/deep.txt")
+file2 = open("model depth/less_layers.txt")
 
 # read files
 file1_acc = [float(x) for x in file1.readline().strip().split(',')]
@@ -19,16 +19,16 @@ file2_val_loss = [float(x) for x in file2.readline().strip().split(',')]
 
 # plot accuracies
 epochs_range = range((len(file1_acc)))
-plt.plot(epochs_range, file1_acc, label="Training Accuracy [Augmentations]")
-plt.plot(epochs_range, file1_val_acc, label="Validation Accuracy [Augmentations]")
-plt.plot(epochs_range, file2_acc, label="Training Accuracy [No augmentations]")
-plt.plot(epochs_range, file2_val_acc, label="Validation Accuracy [No augmentations]")
+plt.plot(epochs_range, file1_acc, label="Training Accuracy [Deep]")
+plt.plot(epochs_range, file1_val_acc, label="Validation Accuracy [Deep]")
+plt.plot(epochs_range, file2_acc, label="Training Accuracy [Shallow]")
+plt.plot(epochs_range, file2_val_acc, label="Validation Accuracy [Shallow]")
 plt.axis(ymin=0.00, ymax=1.2)
 plt.grid()
-plt.title('Model Accuracy')
+plt.title('Model depth and accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epochs')
-plt.legend(['train [Augmentations]', 'validation [Augmentations]', 'train [No augmentations]', 'validation [No augmentations]'])
+plt.legend(['train [Deep]', 'validation [Deep]', 'train [Shallow]', 'validation [Shallow]'])
 plt.show()
 plt.savefig('output-plot.png')
 
